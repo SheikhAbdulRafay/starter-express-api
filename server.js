3cf0f73a443b04293e3b33d74e9e3b5d1071451a
myusername <72190005+SheikhAbdulRafay@users.noreply.github.com>
2023-06-07 14:08:27 +0500

    sd

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     console.log("some request hit")
-    res.send('Hello World!')
+    res.status(200).send('Hello World!')
 })
 
 app.listen(port, () => {
