de3ddf169a802767acdbd2284546ce8fcf40b249
myusername <72190005+SheikhAbdulRafay@users.noreply.github.com>
2023-06-07 14:52:36 +0500

    build added for testing

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"))
-// })
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
 // cron.schedule('* * * * *', () => {
 cron.schedule('0 0 * * *', () => {
   // incrementParent()
