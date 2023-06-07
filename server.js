6e2cb960c88e8422d27b1be1c836f09087591269
myusername <72190005+SheikhAbdulRafay@users.noreply.github.com>
2023-06-07 14:13:44 +0500

    customer api

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/customer', (req, res) => {
     console.log("some request hit")
-    res.status(200).send('Hello World!')
+    res.status(200).send({
+        data: {
+            name: "rafay",
+            "id": "32342342",
+            "phone": "03351900022",
+            "company": "agilevectors"
+        }
+    })
 })
 
 app.listen(port, () => {
